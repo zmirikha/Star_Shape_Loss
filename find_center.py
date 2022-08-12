934d5a68fa44a3241f0ed9df2173ae41915b9df9
Zahra Miri <zahramiri@Zahras-MacBook-Pro.local>
2022-08-12 14:55:58 -0700

    running on cpu

--- find_center.py
+++ find_center.py
@@ -1,125 +1,118 @@
-from __future__ import print_function
 import os
 import numpy as np
 from PIL import Image
 
 
 def unit_vector(vector):
-    
     """ Returns the unit vector of the vector.  """
-    
-    return vector / (np.linalg.norm(vector)+(1e-10))
+
+    return vector / (np.linalg.norm(vector) + np.finfo(np.float32).eps)
+
 
 def cos_between(v1, v2):
-    
     """ Calculate the cosine of the angle between two vectors. """
-    
+
     v1_u = unit_vector(v1)
     v2_u = unit_vector(v2)
     return np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)
 
-def neigh_map(img):
-    
+
+def neigh_map(img,  d=8):
     """
     Find target object center and quantize the possible angles of all lines
     passing through the center to d directions and generate a mask (called
-    neighboring map/ regional map) to split the image domain into d regions.
+    neighboring map/ regional map) to split the image domain into d=8 regions.
     """
-    
-    pos = np.where(img>0)
+
+    theta = np.pi/(d)  # creating sub-regions
+
+    pos = np.where(img > 0)
     center = np.floor(np.mean(pos, axis=1))
 
-    
     size_x = img.shape[0]
     size_y = img.shape[1]
-    
-    v_x = np.zeros((size_x,size_y))
-    v_y = np.zeros((size_x,size_y))
-    ng = np.zeros((size_x,size_y))
-    
+
+    v_x = np.zeros((size_x, size_y))
+    v_y = np.zeros((size_x, size_y))
+    ng = np.zeros((size_x, size_y))
+
     for i in range(size_x):
-     for j in range(size_y):
+        for j in range(size_y):
 
-       v_x[i,j] = center[1]-i
-       v_y[i,j] = center[0]-j
+            v_x[i, j] = center[1] - i
+            v_y[i, j] = center[0] - j
 
-       if (v_x[i,j] >= 0 and v_y[i,j] >= 0):
+            if v_x[i, j] >= 0 and v_y[i, j] >= 0:
 
-          c = cos_between((v_x[i,j],v_y[i,j]), [1,0])
+                c = cos_between((v_x[i, j], v_y[i, j]), [1, 0])
 
-          if (c >= 0.9063 and c <= 1):
-            ng[j,i] = 2
-            
-          elif (c >= 0 and c <= 0.2588):
-            ng[j,i] = 4
-            
-          else:
-            ng[j,i] = 3
+                if np.cos(theta) <= c <= 1:
+                    ng[j, i] = 2
 
-       elif (v_x[i,j] > 0 and v_y[i,j] < 0):
-      
-          c = cos_between((v_x[i,j],v_y[i,j]), [1,0])
+                elif 0 <= c <= np.cos(3*theta):
+                    ng[j, i] = 4
 
-          if (c >= 0.9063 and c <= 1):
-            ng[j,i] = 2
-            
-          elif (c >= 0 and c <= 0.2588):
-            ng[j,i] = 8
-            
-          else:
-            ng[j,i] = 1
+                else:
+                    ng[j, i] = 3
 
-       elif (v_x[i,j] < 0 and v_y[i,j] > 0):
+            elif v_x[i, j] > 0 and v_y[i, j] < 0:
 
-          c = cos_between((v_x[i,j],v_y[i,j]), [1,0])
+                c = cos_between((v_x[i, j], v_y[i, j]), [1, 0])
 
-          if (c <= -0.9063 and c >= -1):
-            ng[j,i] = 6
-            
-          elif (c <= 0 and c >= -0.2588):
-            ng[j,i] = 4
-            
-          else:
-            ng[j,i] = 5
+                if np.cos(theta) <= c <= 1:
+                    ng[j, i] = 2
 
-       elif (v_x[i,j] <= 0 and v_y[i,j] <= 0):
+                elif 0 <= c <= np.cos(3*theta):
+                    ng[j, i] = 8
 
-          c = cos_between((v_x[i,j],v_y[i,j]), [1,0])
+                else:
+                    ng[j, i] = 1
 
-          if (c <= -0.9063 and c >= -1):
-            ng[j,i] = 6
-            
-          elif (c <= 0 and c >= -0.2588):
-            ng[j,i] = 8
-            
-          else:
-            ng[j,i] = 7
+            elif v_x[i, j] < 0 and v_y[i, j] > 0:
+
+                c = cos_between((v_x[i, j], v_y[i, j]), [1, 0])
+
+                if -np.cos(theta) >= c >= -1:
+                    ng[j, i] = 6
+
+                elif 0 >= c >= -np.cos(3*theta):
+                    ng[j, i] = 4
+
+                else:
+                    ng[j, i] = 5
+
+            elif v_x[i, j] <= 0 and v_y[i, j] <= 0:
+
+                c = cos_between((v_x[i, j], v_y[i, j]), [1, 0])
+
+                if -np.cos(theta) >= c >= -1:
+                    ng[j, i] = 6
+
+                elif 0 >= c >= -np.cos(3*theta):
+                    ng[j, i] = 8
+
+                else:
+                    ng[j, i] = 7
 
     return ng
 
 
-
 def main():
-    
     gt_dir = input("enter gt directory:")
     ng_dir = input("enter neighboring maps directory:")
 
     images = os.listdir(gt_dir)
 
     for image in images:
+        gt = Image.open(os.path.join(gt_dir, image))
+        img = np.asarray(gt)
 
-        gt=Image.open(os.path.join(gt_dir, image))
-        img=np.asarray(gt)
-    
         # generate neighbouring maps from target ground truth maps
-        ng=neigh_map(img)
+        ng = neigh_map(img)
 
         ng_img = Image.fromarray(np.uint8(ng))
         ng_img.save(os.path.join(ng_dir, image))
 
 
-
 if __name__ == "__main__":
     main()
-
-
--- star_loss.py
+++ star_loss.py
@@ -7,16 +7,16 @@ from reg_conv import gen_b
 
 
 def total_loss(input, target, ng):
-    ''' 
+    """ 
     Compute the total loss
 
     Args:
     input (torch.Tensor): predicted segmentation probability map
     target (torch.Tensor): binary groundtruth segmentation map
     ng (torch.Tensor): 1-hot encoded neighbouring maps)
-    '''
-    alpha = 1 # CE loss adjustment factor
-    beta = 40 # star shape loss adjustment factor
+    """
+    alpha = 1  # CE loss adjustment factor
+    beta = 40  # star shape loss adjustment factor
     
     l1 = nn.BCELoss()
     l_sh = star_loss(input, target, ng)
@@ -24,17 +24,16 @@ def total_loss(input, target, ng):
     return (l1(input, target) * alpha) + (l_sh * beta)
 
 def star_loss(input, target, ng):
-    ''' 
+    """ 
     Compute the star shape loss
 
     Args:
     input (torch.Tensor): predicted segmentation probability map
     target (torch.Tensor): binary groundtruth segmentation map
     ng (torch.Tensor): 1-hot encoded neighbouring maps)
-
-    '''
+    """
     
-    [dim_0,dim_1] = input.size()
+    [dim_0, dim_1] = input.size()
 
     inp = input.unsqueeze(0)
     tar = target.unsqueeze(0)
@@ -44,21 +43,22 @@ def star_loss(input, target, ng):
         
     b = gen_b(tar) # Convolve gt with regional kernels.
         
-    tmp = torch.sum(torch.mul(ng, b),0) # use the right kernel for each region by one-hot encoded regional maps.   
-    
-    z_eq = Variable(torch.Tensor(1,1,dim_0,dim_1).zero_().cuda())
+    tmp = torch.sum(torch.mul(ng, b), 0)  # use the right kernel for each region by one-hot encoded regional maps.   
 
-    b = torch.eq(tmp.unsqueeze(0),z_eq).float() # compute B_{pq}
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    z_eq = Variable(torch.Tensor(1, 1, dim_0,dim_1).zero_().to(device))
+
+    b = torch.eq(tmp.unsqueeze(0), z_eq).float()  # compute B_{pq}
 
     # Compute if two pixels on a line segment are assigned a same label.
         
-    prob_diff = gen_b(inp) # Convolve predicted map with regional kernels.
+    prob_diff = gen_b(inp)  # Convolve predicted map with regional kernels.
    
-    diff = torch.sum(torch.mul(ng, torch.abs(prob_diff)),0).unsqueeze(0) # use the right kernel for each region by one-hot encoded regional maps. 
+    diff = torch.sum(torch.mul(ng, torch.abs(prob_diff)), 0).unsqueeze(0)  # use the right kernel for each region by one-hot encoded regional maps. 
     
     abs_err = torch.abs(inp.sub(tar.unsqueeze(0)))  #the difference of predicted probabilities and ground truth
     
     # Compute the final star shape loss defined in the paper's eq. 4.
-    loss_per_image = torch.mean(torch.mul(torch.mul(b, diff),abs_err))
+    loss_per_image = torch.mean(torch.mul(torch.mul(b, diff), abs_err))
     
     return loss_per_image
